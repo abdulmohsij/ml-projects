67ae9b35b10e9790029b2cc052dc701251ece655
abdulmohsij <23042596+abdulmohsij@users.noreply.github.com>
2021-03-18 10:08:07 +1030

    Added documentation to the fitting code.

--- fitSinFunction.py
+++ fitSinFunction.py
@@ -24,31 +24,37 @@ from tensorflow.keras.layers import Dense
 
 from sklearn.preprocessing import MinMaxScaler
 
+# Generate the training set.
 x = np.linspace(0, 2 * np.pi, 10000).reshape(-1,1)
 y = np.sin(x)
 
 x_scaler = MinMaxScaler()
 y_scaler = MinMaxScaler()
 
+# Transform the values.
+# See: https://stackoverflow.com/questions/4674623/why-do-we-have-to-normalize-the-input-for-an-artificial-neural-network
 X = x_scaler.fit_transform(x)
 Y = y_scaler.fit_transform(y)
 
+# Create a model using the Sequential API.
 model = Sequential()
 model.add(Dense(16, activation='relu', kernel_initializer='he_uniform', input_dim=1))
 model.add(Dense(16, activation='relu', kernel_initializer='he_uniform'))
 model.add(Dense(1, activation='linear', kernel_initializer='he_uniform'))
 model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
+# Train the model on our scaled data.
 model.fit(X, Y, epochs=20, batch_size=32)
 
+# Test the model on the training data to figure out the training accuracy.
+# Traditionally, we would have a separate test set. In this case, we need to see how our estimated sin function is to the original.
 Y_test = model.predict(X, batch_size=32)
 
+# Convert the predictions back to the original scale.
 Y_res = y_scaler.inverse_transform(Y)
 Y_test_res = y_scaler.inverse_transform(Y_test)
 
-print(Y_res[0:10])
-print(Y_test_res[0:10])
-
+# Plot the results and the difference.
 pl.subplot(211)
 pl.plot(Y_res, label='ann')
 pl.plot(Y_test_res, label='train')
