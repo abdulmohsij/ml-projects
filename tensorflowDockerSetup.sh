4015f7cc71265d9eda78e17081737db7a819b4b6
abdulmohsij <23042596+abdulmohsij@users.noreply.github.com>
2021-03-18 10:30:46 +1030

    Added docker commands for tensorflow container.

---
+++ tensorflowDockerSetup.sh
@@ -0,0 +1,7 @@
+# Install docker and docker gpu support as per: https://www.tensorflow.org/install/docker
+# Then run this script as:
+# source tensorflowDockerSetup.sh
+
+# To start a docker session in your current directory.
+sudo docker run --gpus all --rm -v `pwd`:/my-working-directory -it tensorflow/tensorflow:2.1.0-gpu-py3-jupyter
+cd my-working-directory
