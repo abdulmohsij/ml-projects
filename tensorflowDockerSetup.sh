2d1cd571697dd255e16fb68a27f69483b64a1424
abdulmohsij <23042596+abdulmohsij@users.noreply.github.com>
2021-03-18 10:34:44 +1030

    Added python dependency installation line.

--- tensorflowDockerSetup.sh
+++ tensorflowDockerSetup.sh
@@ -5,3 +5,5 @@
 # To start a docker session in your current directory.
 sudo docker run --gpus all --rm -v `pwd`:/my-working-directory -it tensorflow/tensorflow:2.1.0-gpu-py3-jupyter
 cd my-working-directory
+
+pip install -r requirements.txt
